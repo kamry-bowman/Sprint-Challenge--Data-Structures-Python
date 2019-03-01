fa24736c8183db811bab6121776e74c86dcc228f
Kamry Bowman <kamry.bowman@gmail.com>
2019-03-01 09:55:02 -0700

    answer questions

--- Data_Structures_Answers.md
+++ Data_Structures_Answers.md
@@ -1,18 +1,17 @@
 Add your answers to the questions below.
 
-1. What is the runtime complexity of your `depth_first_for_each` method?
+1. What is the runtime complexity of your `depth_first_for_each` method? O(n), as it must traverse the full binary structure
 
-2. What is the space complexity of your `depth_first_for_each` function?
+2. What is the space complexity of your `depth_first_for_each` function? O(k), where k is the maximum depth of the tree, as that is how full the stack could get
 
 3. What is the runtime complexity of your `breadth_first_for_each` method?
 
 4. What is the space complexity of your `breadth_first_for_each` method?
 
+5) What is the runtime complexity of the provided code in `names.py`? O(n^2), two nested loops
 
-5. What is the runtime complexity of the provided code in `names.py`?
+6) What is the space complexity of the provided code in `names.py`? O(n), 2 lists fitting the full amount of input
 
-6. What is the space complexity of the provided code in `names.py`?
+7) What is the runtime complexity of your optimized code in `names.py`? O(n) I think, using dictionary, assuming O(1) lookup in dictionaries
 
-7. What is the runtime complexity of your optimized code in `names.py`?
-
-8. What is the space complexity of your optimized code in `names.py`?
+8) What is the space complexity of your optimized code in `names.py`? O(n), will make a dictionary the size of the input
--- names/names.py
+++ names/names.py
@@ -1,17 +1,65 @@
 import time
 
+
+class BinarySearchTree:
+    def __init__(self, value=None):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        if self.value == None:
+            self.value = value
+        else:
+            target = 'left' if value <= self.value else 'right'
+            target_val = getattr(self, target)
+            if target_val is None:
+                setattr(self, target, BinarySearchTree(value))
+            else:
+                target_val.insert(value)
+
+    def contains(self, target):
+        if self.value == target:
+            return True
+        if target < self.value:
+            return self.left and self.left.contains(target)
+        else:
+            return self.right and self.right.contains(target)
+
+    def get_max(self):
+        if self.right is None:
+            return self.value
+        else:
+            return self.right.get_max()
+
+
 start_time = time.time()
 
+# binary tree version
+# names = BinarySearchTree()
+# duplicates = []
+# with open('names_1.txt', 'r') as f:
+#     for line in f.readlines():
+#         names.insert(line.strip())
+
+# with open('names_2.txt', 'r') as f:
+#     for line in f.readlines():
+#         line = line.strip()
+#         if names.contains(line):
+#             duplicates.append(line)
+
+# dictionary version
 names = {}
 duplicates = []
 with open('names_1.txt', 'r') as f:
     for line in f.readlines():
-        names[line] = 1
+        names[(line.strip())] = 1
 
 with open('names_2.txt', 'r') as f:
     for line in f.readlines():
+        line = line.strip()
         if names.get(line) is not None:
-            duplicates.append(line.strip())
+            duplicates.append(line)
 
 
 end_time = time.time()
