2c90495c305998d08b638f3530256abb46bc035b
Kamry Bowman <kamry.bowman@gmail.com>
2019-03-01 09:24:44 -0700

    attempt names.py

--- names/names.py
+++ names/names.py
@@ -2,21 +2,18 @@ import time
 
 start_time = time.time()
 
-f = open('names_1.txt', 'r')
-names_1 = f.read().split("\n")  # List containing 10000 names
-f.close()
-
-f = open('names_2.txt', 'r')
-names_2 = f.read().split("\n")  # List containing 10000 names
-f.close()
-
+names = {}
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+with open('names_1.txt', 'r') as f:
+    for line in f.readlines():
+        names[line] = 1
+
+with open('names_2.txt', 'r') as f:
+    for line in f.readlines():
+        if names.get(line) is not None:
+            duplicates.append(line)
+
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
-
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
